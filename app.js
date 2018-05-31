8ce8c3cb3887364f17f5384a9fde69938cefaeb5
Santiago Gigli <santigigli93@gmail.com>
2018-05-31 12:30:04 -0300

    add a function to the play button

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.console.log("hello");
+console.log("hello");
+
+var changeColor = function (li) {
+  console.log(li);
+  li.style.backgroundColor = "#F00";
+}
+
+window.onload = function () {
+  var btnPlay = document.getElementById('play');
+  btnPlay.onclick = function () {
+  var liAlives = document.getElementsByClassName('alive');
+  for (var i = 0; i < liAlives.length; i++) {
+    changeColor(liAlives[i]);
+  }
+
+  }
+
+}
